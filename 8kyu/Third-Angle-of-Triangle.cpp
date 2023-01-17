dc54ed24c2c92b70c03e8495ac29ba0a3eda1d19
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-01-17 19:05:20 +0800

    Add files via upload

---
+++ 8kyu/Third-Angle-of-Triangle.cpp
@@ -0,0 +1,22 @@
+/*
+
+
+
+*/
+class Triangle {
+    public:
+    static int otherAngle(int a, int b) {
+        return 180 - (a + b);
+    }
+};
+/*
+
+TEST RESULTS:
+        Time: 2884ms Passed: 2Failed: 0
+        Test Results:
+        sampleTests
+        shouldBe
+        randomTests
+        shouldBe
+        You have passed all of the tests! :)
+*/
