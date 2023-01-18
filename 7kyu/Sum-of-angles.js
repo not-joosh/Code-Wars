8f37eab390499830fe5d83d9bdce90ed82a23df0
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-01-18 08:26:00 +0800

    Add files via upload

---
+++ 7kyu/Sum-of-angles.js
@@ -0,0 +1,27 @@
+/*
+        7 kyu
+        Sum of angles
+        1375080% of 2,7615,765 of 24,252DemonKing2 Issues Reported
+        JavaScript
+        Node v18.x
+        VIM
+        EMACS
+        Instructions
+        Output
+        Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+function angle(n) {
+    return (n-2) * 180;
+}
+/*
+TEST RESULTS:
+        Time: 736ms Passed: 2Failed: 0
+        Test Results:
+        Basic tests
+        Testing for fixed tests
+        Completed in 1ms
+        Random tests
+        Testing for 40 random tests
+        Completed in 1ms
+        You have passed all of the tests! :)
+*/
