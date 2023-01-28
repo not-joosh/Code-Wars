d98c12c79f6c27df8dacb54691e306a3b7dadb70
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-01-28 09:13:50 +0800

    Add files via upload

---
+++ 8kyu/Convert-a-Boolean-to-a-String.java
@@ -0,0 +1,31 @@
+/*
+
+    8 kyu
+    Convert a Boolean to a String
+    4389687% of 8,60248,953 of 124,940btaitelb
+    Java
+    17
+    VIM
+    EMACS
+    Instructions
+    Output
+    Implement a function which convert the given boolean value into its string representation.
+    Note: Only valid inputs will be given.
+
+*/
+public class BooleanToString {
+  public static String convert(boolean b) {
+    return "" + b;
+  }
+}
+/*
+TEST RESULTS:
+    Time: 2745ms Passed: 3Failed: 0
+    Test Results:
+    BoolToStrTest
+    testFalse()
+    testTrue()
+    randomTests()
+    Completed in 55ms
+    You have passed all of the tests! :)
+*/
