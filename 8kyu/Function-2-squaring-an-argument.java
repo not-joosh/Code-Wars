2b5f0c6212186972f35e8c812002b3096263a1f6
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-01-29 11:28:38 +0800

    Add files via upload

---
+++ 8kyu/Function-2-squaring-an-argument.java
@@ -0,0 +1,29 @@
+/*
+        8 kyu
+        Function 2 - squaring an argument
+        1201687% of 3,7371,042 of 44,370ineiti2 Issues Reported
+        Java
+        17
+        VIM
+        EMACS
+        Instructions
+        Output
+        Now you have to write a function that takes an argument and returns the square of it.
+ */
+public class Kata {
+    public static int square(int n){
+        return n*n;
+    }
+ }
+ /*
+TEST RESULTS:
+        Time: 2099ms Passed: 7Failed: 0
+        Test Results:
+        SolutionTest
+        random_tests()
+        random_tests()
+        fixed_tests()
+        Completed in 62ms
+        STDERR
+        You have passed all of the tests! :)
+*/
