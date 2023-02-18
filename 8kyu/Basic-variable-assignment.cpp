5d8dbe603078342d129a9cae1158461cfe7c907f
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-18 18:28:08 +0800

    Add files via upload

---
+++ 8kyu/Basic-variable-assignment.cpp
@@ -0,0 +1,31 @@
+/*
+
+        8 kyu
+        Basic variable assignment
+        1322079% of 4,2521,943 of 39,220Ira2 Issues Reported
+        C++
+        Clang 8 C++17
+        VIM
+        EMACS
+        Instructions
+        Output
+        This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+*/
+#include <string>
+
+std::string a = "code";
+std::string b = "wa.rs";
+std::string name = a + b;
+/*
+TEST RESULTS:
+        8 kyu
+        Basic variable assignment
+        1322079% of 4,2521,943 of 39,220Ira2 Issues Reported
+        C++
+        Clang 8 C++17
+        VIM
+        EMACS
+        Instructions
+        Output
+*/
