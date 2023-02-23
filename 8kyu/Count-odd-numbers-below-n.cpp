014521a9401787264b669d07e7c08904bfcd2fa0
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-23 20:10:40 +0800

    Add files via upload

---
+++ 8kyu/Count-odd-numbers-below-n.cpp
@@ -0,0 +1,31 @@
+/*
+        8 kyu
+        Count Odd Numbers below n
+        4259588% of 4,6202,284 of 37,663kodejuice
+        C++
+        Clang 8 C++17
+        VIM
+        EMACS
+        Instructions
+        Output
+        Given a number n, return the number of positive odd numbers below n, EASY!
+
+        Examples (Input -> Output)
+        7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+        15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+        Expect large Inputs!
+*/
+int odd_count(int n){
+    return n / 2;
+}
+/*
+TEST RESULTS:
+        Time: 2296ms Passed: 3Failed: 0
+        Test Results:
+        basic_tests
+        test1
+        test2
+        Random_tests
+        randomized_test
+        You have passed all of the tests! :)
+*/
