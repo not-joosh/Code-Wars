a3e84c238970b0a21615841dfc9d416cd0f7a4f3
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-27 18:10:42 +0800

    Add files via upload

---
+++ 8kyu/You-cant-code-under-pressure-1.go
@@ -0,0 +1,27 @@
+/*
+		8 kyu
+		You Can't Code Under Pressure #1
+		2923378% of 9,0492,382 of 130,520Kerrick2 Issues Reported
+		Go
+		1.18
+		VIM
+		EMACS
+		Instructions
+		Output
+		Code as fast as you can! You need to double the integer and return it.
+*/
+package kata
+
+func DoubleInteger(i int) int {
+    return 2 * i
+}
+/*
+TEST RESULTS:
+		Time: 2494ms Passed: 2Failed: 0
+		Test Results:
+		Random Tests
+		Should pass random tests
+		Test Example
+		should work for some examples
+		You have passed all of the tests! :)
+*/
