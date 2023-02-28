edd981f64b70954a1963d6f05c68cf9e42111c11
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-28 22:31:47 +0800

    Add files via upload

---
+++ 8kyu/Return-to-sanity.ts
@@ -0,0 +1,25 @@
+/*
+        8 kyu
+        Return to Sanity
+        551779% of 1,74691 of 16,881begriffs
+        TypeScript
+        4.2
+        VIM
+        EMACS
+        Instructions
+        Output
+        This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+export function mystery(): object {
+    const results = { sanity: 'Hello' };
+    return results;
+}
+/*
+TEST RESULTS:
+        Time: 3693ms Passed: 1Failed: 0
+        Test Results:
+        example
+        test
+        Completed in 2ms
+        You have passed all of the tests! :)
+*/
