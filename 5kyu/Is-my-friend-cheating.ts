19e1caca418030b3ec4beb13981fb01094244289
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-03 22:56:12 +0800

    Add files via upload

---
+++ 5kyu/Is-my-friend-cheating.ts
@@ -0,0 +1,64 @@
+/*
+        5 kyu
+        Is my friend cheating?
+        165028889% of 1,946376 of 15,048g9644 Issues Reported
+        TypeScript
+        4.9
+        VIM
+        EMACS
+        Instructions
+        Output
+        A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+        Within that sequence, he chooses two numbers, a and b.
+        He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+        Given a number n, could you tell me the numbers he excluded from the sequence?
+        The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+
+        [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+        with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+
+        [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the "a".
+
+        It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+
+        Examples:
+        removNb(26) should return [(15, 21), (21, 15)]
+        or
+        removNb(26) should return { {15, 21}, {21, 15} }
+        or
+        removeNb(26) should return [[15, 21], [21, 15]]
+        or
+        removNb(26) should return [ {15, 21}, {21, 15} ]
+        or
+        removNb(26) should return "15 21, 21 15"
+        or
+
+        in C:
+        removNb(26) should return  {{15, 21}{21, 15}} tested by way of strings.
+        Function removNb should return a pointer to an allocated array of Pair pointers, each one also allocated. 
+        Note
+        See examples of returns for each language in "RUN SAMPLE TESTS"
+*/
+export function removeNb(n: number): number[][] {
+    const sum = (n * (n + 1)) / 2;
+    const result: number[][] = [];
+    for (let a = 1; a <= n; a++) {
+        const b = (sum - a) / (a + 1);
+        if (Number.isInteger(b) && b >= 1 && b <= n && a * b === sum - a - b) {
+            result.push([a, b]);
+        }
+    }
+    return result;
+}
+/*
+TEST RESULTS:
+        Time: 2922ms Passed: 4Failed: 0
+        Test Results:
+        Fixed Tests removeNb
+        Basic tests
+        should work when the friend is cheating
+        should work for higher numbers
+        should work for some random numbers
+        Completed in 21ms
+        You have passed all of the tests! :)
+*/
