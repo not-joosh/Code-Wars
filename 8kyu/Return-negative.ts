c747aecefb437684a9efbee3d3766b0f7ee67d26
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-05 18:23:39 +0800

    Add files via upload

---
+++ 8kyu/Return-negative.ts
@@ -0,0 +1,33 @@
+/*
+        8 kyu
+        Return Negative
+        93222091% of 17,9542,582 of 231,450Deantwo
+        TypeScript
+        4.9
+        VIM
+        EMACS
+        Instructions
+        Output
+        In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+        Examples
+        makeNegative(1);  // return -1
+        makeNegative(-5); // return -5
+        makeNegative(0);  // return 0
+        Notes
+        The number can be negative already, in which case no change is required.
+        Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+export const makeNegative = (num: number): number => {
+    return num >= 0 ? -num : num;
+};
+/*
+TEST RESULT:
+        Time: 3384ms Passed: 2Failed: 0
+        Test Results:
+        makeNegative
+        Sample tests
+        Random tests
+        Completed in 2ms
+        You have passed all of the tests! :)
+*/
