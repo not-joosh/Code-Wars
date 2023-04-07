3fddf348215eb71cd5ba93b3b831cfa6f6c0fab5
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-07 16:03:02 +0800

    Add files via upload

---
+++ 8kyu/Volume-of-Cuboid.ts
@@ -0,0 +1,25 @@
+/*
+        8 kyu
+        Volume of a Cuboid
+        2075379% of 5,972351 of 68,101user51964771 Issue Reported
+        TypeScript
+        4.9
+        VIM
+        EMACS
+        Instructions
+        Output
+        Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+*/
+export function getVolumeOfCuboid(length: number, width: number, height: number): number {
+    return length * width * height;
+}
+/*
+TEST RESULTS:
+        Time: 3913ms Passed: 2Failed: 0
+        Test Results:
+        Some testing
+        Sample tests
+        Random tests
+        Completed in 1ms
+        You have passed all of the tests! :)
+*/
