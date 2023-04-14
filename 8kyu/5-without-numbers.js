b8bb1a5cf8d82eca3dbac0257417228427c63e85
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-14 22:56:53 +0800

    Add files via upload

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,30 @@
+/*
+        8 kyu
+        5 without numbers !!
+        2348292% of 3,1999,703 of 23,939KH!
+        JavaScript
+        Node v18.x
+        VIM
+        EMACS
+        Instructions
+        Output
+        Write a function that always returns 5
+
+        Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+        Good luck :)
+*/
+function unusualFive() {
+    return 'fizzz'.length;
+};
+/*
+TEST RESULTS:
+        Time: 884ms Passed: 2Failed: 0
+        Test Results:
+        cheat test
+        should not use any banned characters ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/")
+        Completed in 2ms
+        unusualFive
+        should return 5
+        You have passed all of the tests! :)
+*/
