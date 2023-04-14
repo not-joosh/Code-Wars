379d5eca8af985249842ffc7fe41ae738095d49a
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-14 22:57:27 +0800

    Add files via upload

---
+++ 8kyu/Sum-arrays.cpp
@@ -0,0 +1,46 @@
+/*
+        8 kyu
+        Sum Arrays
+        51113091% of 7,9154,020 of 108,150richardhsu3 Issues Reported
+        C++
+        Clang 8 C++17
+        VIM
+        EMACS
+        Instructions
+        Output
+        Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+        Examples
+        Input: [1, 5.2, 4, 0, -1]
+        Output: 9.2
+
+        Input: []
+        Output: 0
+
+        Input: [-2.398]
+        Output: -2.398
+
+        Assumptions
+        You can assume that you are only given numbers.
+        You cannot assume the size of the array.
+        You can assume that you do get an array and if the array is empty, return 0.
+        What We're Testing
+        We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+        Advanced users may find this extremely easy and can easily write this in one line.
+*/
+double sum(std::vector<int> nums) {
+    double total = 0;
+    for (int num : nums) {
+        total += num;
+    }
+    return total;
+}
+/*
+TEST RESULT:
+        Time: 2366ms Passed: 2Failed: 0
+        Test Results:
+        SummingArrays
+        FixedTests
+        RandomTests
+        You have passed all of the tests! :)
+*/
