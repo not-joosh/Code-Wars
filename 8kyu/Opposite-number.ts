7da9d4019c68cd9a24da617498810bf6d8de881e
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-16 09:41:16 +0800

    Add files via upload

---
+++ 8kyu/Opposite-number.ts
@@ -0,0 +1,34 @@
+/*
+        8 kyu
+        Opposite number
+        93028484% of 19,3482,257 of 256,814sergioet1 Issue Reported
+        TypeScript
+        4.9
+        VIM
+        EMACS
+        Instructions
+        Output
+        Very simple, given an integer or a floating-point number, find its opposite.
+
+        Examples:
+
+        1: -1
+        14: -14
+        -34: 34
+*/
+export class Kata {
+    static opposite(n: number) {
+        return (n*-1);
+    };
+};
+/*
+TEST RESULT:
+        Time: 3708ms Passed: 3Failed: 0
+        Test Results:
+        opposite
+        Sample Test
+        Fixed Tests
+        Random Tests
+        Completed in 4ms
+        You have passed all of the tests! :)
+*/
