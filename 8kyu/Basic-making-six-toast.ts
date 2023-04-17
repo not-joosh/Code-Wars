f33d57fc91ee801c1f1691f9810fa5e4e70a32f3
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-04-17 18:45:19 +0800

    Add files via upload

---
+++ 8kyu/Basic-making-six-toast.ts
@@ -0,0 +1,40 @@
+/*
+        8 kyu
+        BASIC: Making Six Toast.
+        592688% of 1,983230 of 10,007user3186707
+        TypeScript
+        4.9
+        VIM
+        EMACS
+        Instructions
+        Output
+        Story:
+        You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+
+        Problem:
+        You forgot to count the number of toast you put into there, you don't know if you put exactly six pieces of toast into the toasters.
+
+        Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative.
+
+        Examples:
+        You must return the number of toast the you need to put in (or to take out). In case of 5 you can still put 1 toast in:
+
+        six_toast(5) == 1
+        And in case of 12 you need 6 toasts less (but not -6):
+
+        six_toast(12) == 6
+        Good luck!
+*/
+export function sixToast(num:number):number {
+    return (num - 6 < 0)? (num - 6) * -1: (num - 6);
+};
+/*
+TEST RESULT:
+        Time: 3086ms Passed: 2Failed: 0
+        Test Results:
+        Tests
+        Basic Tests
+        Random Tests
+        Completed in 2ms
+        You have passed all of the tests! :)
+*/
