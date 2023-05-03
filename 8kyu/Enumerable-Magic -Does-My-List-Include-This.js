e12aadc50b19510d3682f09d73d3871487107050
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-05-03 21:56:10 +0800

    Add files via upload

---
+++ 8kyu/Enumerable-Magic -Does-My-List-Include-This.js
@@ -0,0 +1,25 @@
+/*
+        8 kyu
+        Enumerable Magic - Does My List Include This?
+        924492% of 2,3786,781 of 17,590bellmyer2 Issues Reported
+        JavaScript
+        Node v18.x
+        VIM
+        EMACS
+        Instructions
+        Output
+        Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+function include(arr, item) {
+    return arr.includes(item);
+}
+/*
+TEST RESULTS:
+        Time: 819ms Passed: 2Failed: 0
+        Test Results:
+        Tests
+        Fixed tests
+        Random tests
+        Completed in 4ms
+        You have passed all of the tests! :)
+*/
